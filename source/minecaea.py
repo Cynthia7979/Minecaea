010b7455edb28ea406fdeb9e645e47a435522014
EnderKerman <anatominuniverse@gmail.com>
2019-02-16 17:08:43 +0800

    created as main file

---
+++ source/minecaea.py
@@ -0,0 +1,14 @@
+from File_Load import *
+from OOP import *
+import sys
+sys.path.insert(0,'C:/Users/Administrator/Desktop/Experimental Server/Minecaea/libs')
+from mcpi import minecraft
+
+lane_width = 24
+whr = 2
+
+chart = main('test.aff')
+notes = chart.get_chart()
+
+
+
