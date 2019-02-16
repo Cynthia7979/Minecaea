57283d11e2184108bd7badf32b22d9d83d49b659
EnderKerman <anatominuniverse@gmail.com>
2019-02-16 23:05:42 +0800

    minor edites and connection to minecraft

--- source/minecaea.py
+++ source/minecaea.py
@@ -1,15 +1,15 @@
 from file_load import *
 from articles import *
-import sys
 from mcpi import minecraft
 
-sys.path.insert(0,'C:/Users/Administrator/Desktop/Experimental Server/Minecaea/libs')
-
+#almost constant variables
 lane_width = 24
 whr = 2
 
 #chart = main('test.aff')
 #notes = chart.get_chart()
 
-
-
+#get original point
+mc=minecraft.Minecraft.create()
+pos=mc.player.getTilePos()
+x0, y0, z0 = pos.x, pos.y, pos.z
