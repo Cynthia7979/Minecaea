74e2525b1137143896775fe5f0b211499a01b5e5
EnderKerman <40514041+EnderKerman@users.noreply.github.com>
2019-03-13 10:05:54 +0800

    adjust the floor length to chart length

--- source/minecaea.py
+++ source/minecaea.py
@@ -8,7 +8,7 @@ LANE_WIDTH = 10
 WHR = 2.5  # width-height ratio
 DEBUG_OUTPUT = False
 Y_SCALE = LANE_WIDTH * 5 / WHR
-Z_SCALE = 10 ** (-3.5)
+Z_SCALE = 5e-4
 FILE = "2.aff"
 
 
@@ -19,15 +19,24 @@ def main():
     pos = mc.player.getTilePos()
     x0, y0, z0 = pos.x, pos.y, pos.z
     rotation = [[1,0],[0,1]] #currently has no function, unless being assigned according to the player's rotation
+    
+    music_chart = file_load.load(FILE)
+    music_chart.build(LANE_WIDTH, Y_SCALE, Z_SCALE)
+    # get last block
+    try:
+        last = music_chart.all_blocks[-1].t2
+    except:
+        last = music_chart.all_blocks[-1].t1
+    last_block = music_chart.t2z(last)
+    
     for i in range(0, 3050, 10):  # Clear space
         mc.setBlocks(x0 - LANE_WIDTH, y0, z0 + i, x0 + 4 + (LANE_WIDTH * 3), y0 + 100, z0 + i + 9, blocks.AIR)
         sleep(0.01)  # Prevent server crash
     mc.setBlock(x0, y0, z0, blocks.WOOL.id, 14)  # Test, also original coordinate
     mc.setBlocks(x0 - LANE_WIDTH, y0 - 1, z0, x0 + 4 + (LANE_WIDTH * 3), y0 - 1, z0 + 3050, blocks.IRON_BLOCK)
+    # TODO: multiply the rotation matrix to the floor
     for i in range(3):
-        mc.setBlocks(x0 + i * LANE_WIDTH + i + 1, y0 - 1, z0, x0 + i * LANE_WIDTH + i + 1, y0 - 1, z0 + 2150, blocks.WOOL, 1)
-    music_chart = file_load.load(FILE)
-    music_chart.build(LANE_WIDTH, Y_SCALE, Z_SCALE)
+        mc.setBlocks(x0 + i * LANE_WIDTH + i + 1, y0 - 1, z0, x0 + i * LANE_WIDTH + i + 1, y0 - 1, z0 + last_block, blocks.WOOL, 1)
     for block in music_chart.all_blocks:
         x, y, z, block_to_set, data = block.values()
         x, z = x*rotation[0][0]+z*rotation[0][1], x*rotation[1][0]+z*rotation[1][1] # linear algrebra saves me!
