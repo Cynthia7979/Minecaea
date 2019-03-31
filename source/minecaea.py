fixed floor rotation; continuous rotation still wrong

source/minecaea.py
@@ -9,7 +9,7 @@ LANE_WIDTH = 10
 WHR = 2.5  # width-height ratio
 DEBUG_OUTPUT = False
 Y_SCALE = LANE_WIDTH * 5 / WHR
-Z_SCALE = 1e-6
+Z_SCALE = 1e-5
 FILE = "0.aff"
 CONTINUOUS_ROTATION = False
 
@@ -32,15 +32,15 @@ def main():
     if rot <45 or rot >315:
         rotation = [[-1,0],[0,1]]
     elif rot<135:
-        rotation = [[0,-1],[-1,0]]
+        rotation = [[0,1],[1,0]]
     elif rot<225:
         rotation = [[1,0],[0,-1]]
     else:
-        rotation = [[0,1],[1,0]]
+        rotation = [[0,-1],[-1,0]]
         
     theta = rot/360 * 2*np.pi
     if CONTINUOUS_ROTATION == True:
-        rotation = [[-np.cos(theta),-np.sin(theta)],[-np.sin(theta),np.cos(theta)]]
+        rotation = [[-np.cos(theta),np.sin(theta)],[np.sin(theta),np.cos(theta)]]
     
     music_chart = file_load.load(FILE)
     music_chart.build(LANE_WIDTH, Y_SCALE, Z_SCALE)
@@ -55,22 +55,18 @@ def main():
     '''
     
     mc.setBlock(x0, y0, z0, blocks.WOOL.id, 14)  # Test, also original coordinate
-    fl_x0,fl_z0,fl_x1,fl_z1=x0 - LANE_WIDTH,z0,x0 + 4 + (LANE_WIDTH * 3),z0 + last
+    fl_x0,fl_z0,fl_x1,fl_z1=- LANE_WIDTH,0,4 + (LANE_WIDTH * 3),last+10
     floor_coords=[[fl_x0,fl_z0],[fl_x1,fl_z1]]
     floor_coords=mat_product(rotation,floor_coords)
-    mc.setBlocks(floor_coords[0][0], y0 - 1, floor_coords[0][1], floor_coords[1][0], y0 - 1, floor_coords[1][1], blocks.IRON_BLOCK)
-    '''
+    mc.setBlocks(floor_coords[0][0]+x0, y0 - 1, floor_coords[0][1]+z0, floor_coords[1][0]+x0, y0 - 1, floor_coords[1][1]+z0, blocks.IRON_BLOCK)
+    
     
     lane_border = []
     for i in range(3):
-        lane_border=[[x0 + i * LANE_WIDTH + i + 1, z0],[x0 + i * LANE_WIDTH + i + 1, z0 + last_block]]
+        lane_border=[[ i * LANE_WIDTH + i + 1, 0],[i * LANE_WIDTH + i + 1,last+10]]
         lane_border=mat_product(rotation,lane_border)
-    '''
-    # TODO: mat product for lane borders
-    '''
-    for i in range(3):
-        mc.setBlocks(x0 + i * LANE_WIDTH + i + 1, y0 - 1, z0, x0 + i * LANE_WIDTH + i + 1, y0 - 1, z0 + last, blocks.WOOL, 1)
-    '''
+        mc.setBlocks(x0 + lane_border[0][0], y0 - 1, z0+ lane_border[0][1], x0 + lane_border[1][0], y0 - 1, z0 + + lane_border[1][1], blocks.WOOL, 1)
+    
     for block in music_chart.all_blocks:
         x, y, z, block_to_set, data = block.values()
         xz = mat_product(rotation, [[x,z]])#x*rotation[0][0]+z*rotation[0][1], x*rotation[1][0]+z*rotation[1][1] # linear algrebra saves me!